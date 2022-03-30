f9d14ab4c9006b1bb054550d6693503a804b992e
Rob Masters <rjcmasters@gmail.com>
2022-03-30 17:02:09 +0100

    updated to React 18 createRoot api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import RouteSwitch from './RouteSwitch';
 
-const rootElement = document.getElementById('root');
+const container = document.getElementById('root');
 
-ReactDOM.render(
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <RouteSwitch />
     </BrowserRouter>
   </React.StrictMode>,
-  rootElement
 );
