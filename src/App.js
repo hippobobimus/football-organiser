83212120536a6e5ec89b8985a13aa77c0104912c
Rob Masters <rjcmasters@gmail.com>
2022-03-31 16:45:50 +0100

    added global variables

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import uniqid from 'uniqid';
 import Navbar from './components/Navbar';
+import globals from './globals.js';
 import theme from './theme.js';
 
 function App() {
-  const menuItems = [
-    { uid: uniqid(), text: 'Home', path: '/' },
-    { uid: uniqid(), text: 'Lineup', path: '/lineup' },
-    { uid: uniqid(), text: 'Weather', path: '/weather' },
-    { uid: uniqid(), text: 'Location', path: '/location' },
-    { uid: uniqid(), text: 'FAQ', path: '/faq' },
-  ];
-
   return (
     <ThemeProvider theme={theme}>
       <div>
-        <Navbar title='Bib Game Players' menuItems={menuItems}/>
+        <Navbar title={globals.title} menuItems={globals.menuItems}/>
         <Outlet />
       </div>
     </ThemeProvider>
---
+++ src/globals.js
@@ -0,0 +1,14 @@
+import uniqid from 'uniqid';
+
+const globals = {
+  title: 'Bib Game Players',
+  menuItems: [
+    { uid: uniqid(), text: 'Home', path: '/' },
+    { uid: uniqid(), text: 'Lineup', path: '/lineup' },
+    { uid: uniqid(), text: 'Weather', path: '/weather' },
+    { uid: uniqid(), text: 'Location', path: '/location' },
+    { uid: uniqid(), text: 'FAQ', path: '/faq' },
+  ],
+};
+
+export default globals;
