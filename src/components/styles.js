e22b3c3f4b92dc56a0a2f2a32e0237425c4aa5e8
Rob Masters <rjcmasters@gmail.com>
2022-04-05 14:24:43 +0100

    added Card, Content and Subtitle styles

--- src/components/styles.js
+++ src/components/styles.js
@@ -6,6 +6,35 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const Card = styled(Container)`
+  flex-direction: column;
+
+  background: repeating-linear-gradient(
+    45deg,
+    ${(props) => props.theme.fgClr},
+    ${(props) => props.theme.fgClr} 80px,
+    ${(props) => props.theme.fgClrDark} 80px,
+    ${(props) => props.theme.fgClrDark} 160px
+  );
+  border-radius: 25px;
+  box-shadow: 3px 3px 2px ${(props) => props.theme.boxShadowClr};
+  padding: 20px;
+
+  min-width: 100%;
+  min-height: 90%;
+`;
+
+const Content = styled(Container)`
+  grid-row: 2 / 3;
+  grid-column: 1 / 2;
+  height: 100%;
+  padding: 20px min(10%, 5000px);
+  flex-direction: column;
+  justify-content: flex-start;
+
+  background-color: ${(props) => props.theme.bgClr};
+`;
+
 const Icon = styled.img`
   height: 1rem;
 
@@ -21,4 +50,12 @@ const Title = styled.h1`
   white-space: nowrap;
 `;
 
-export { Container, Icon, Title };
+const Subtitle = styled.h2`
+  color: ${(props) => props.theme.textClr};
+  font-family: ${(props) => props.theme.subtitleFont};
+  font-size: 1.8rem;
+  white-space: nowrap;
+  align-text: center;
+`;
+
+export { Card, Container, Content, Icon, Subtitle, Title };
