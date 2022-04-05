55dbfd4fcc6dd149afec74c0c8a19fdd8eba6fee
Rob Masters <rjcmasters@gmail.com>
2022-04-05 14:26:23 +0100

    added card to hold content

--- src/routes/MatchLineup.js
+++ src/routes/MatchLineup.js
@@ -1,6 +1,21 @@
+import { Card } from '../components/styles';
+
 function Lineup() {
   return (
-    <p>Lineup</p>
+    <Card>
+      <ul>
+        <li>John Smith</li>
+        <li>Jane Doe</li>
+        <li>Tom Jones</li>
+        <li>Nancy Yates</li>
+        <li>Troy Garcia</li>
+        <li>Lucy Phelps</li>
+        <li>Brandon Bell</li>
+        <li>Gerry Glover</li>
+        <li>Bruce McCoy</li>
+        <li>Phil Long</li>
+      </ul>
+    </Card>
   );
 }
 
