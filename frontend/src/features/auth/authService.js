78a6d9235176eae21cefe84fa6b40b638eeb15cf
Rob Masters <rjcmasters@gmail.com>
2022-05-09 16:45:37 +0100

    added login and logout service methods

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,27 +1,52 @@
 // TODO make domain dynamic
 const API_URL = 'http://localhost:5000/api/users/';
 
-export const register = async (userData) => {
+const register = async (userData) => {
   // does not reject on http error status codes, must be handled separately.
   const response = await fetch(API_URL, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
 
   const data = await response.json();
 
-  if (response.ok) {
-    localStorage.setItem('user', JSON.stringify(data.user));
-    return data;
-  } else {
+  if (!response.ok) {
     throw new Error(data.message);
   }
+
+  localStorage.setItem('user', JSON.stringify(data.user));
+  return data;
 };
 
-const authService = { register };
+const login = async (userData) => {
+  // does not reject on http error status codes, must be handled separately.
+  const response = await fetch(API_URL + 'login', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+
+  localStorage.setItem('user', JSON.stringify(data.user));
+  return data;
+};
+
+const logout = async () => {
+  localStorage.removeItem('user');
+};
+
+const authService = { register, login, logout };
 
 export default authService;
