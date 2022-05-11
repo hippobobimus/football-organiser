a101b267e7faf455476f93f25cca746f196f7d1c
Rob Masters <rjcmasters@gmail.com>
2022-05-11 10:00:10 +0100

    added a proxy to forward api requests in development

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,5 +1,4 @@
-// TODO make domain dynamic
-const API_URL = 'http://localhost:5000/api/users/';
+const API_URL = '/api/users/';
 
 const register = async (userData) => {
   const response = await fetch(API_URL, {
