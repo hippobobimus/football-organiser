1dc8311c389c3ad40c526f1a518c57ad4e2bc58b
Rob Masters <rjcmasters@gmail.com>
2022-05-12 10:06:16 +0100

    updated validation middlewares to arrow functions in order to correct problem with not applying existence check

--- backend/src/middleware/validation.js
+++ backend/src/middleware/validation.js
@@ -1,47 +1,67 @@
 import { body } from 'express-validator';
 
-const firstName = body('firstName')
-  .trim()
-  .isLength({ min: 1, max: 100 })
-  .escape()
-  .withMessage('First name must be specified.')
-  .isAlphanumeric('en-GB', { ignore: "'-" })
-  .withMessage(
-    'First name has invalid characters (a-z, A-Z, hyphenation and apostrophes only).'
-  );
+// If the validator is exported directly the '.exists()' check will fail.
+// Hence exported as an arrow function!
 
-const lastName = body('lastName')
-  .trim()
-  .isLength({ min: 1, max: 100 })
-  .escape()
-  .withMessage('Last name must be specified.')
-  .isAlphanumeric('en-GB', { ignore: "'-" })
-  .withMessage(
-    'Last name has invalid characters (a-z, A-Z, hyphenation and apostrophes only).'
-  );
+const firstName = () => {
+  return body('firstName')
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .escape()
+    .withMessage('First name must be specified.')
+    .bail()
+    .isAlphanumeric('en-GB', { ignore: "'-" })
+    .withMessage(
+      'First name has invalid characters (a-z, A-Z, hyphenation and apostrophes only).'
+    );
+};
 
-const email = body('email')
-  .isEmail()
-  .withMessage('A valid email address must be provided.');
+const lastName = () => {
+  return body('lastName')
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .escape()
+    .withMessage('Last name must be specified.')
+    .bail()
+    .isAlphanumeric('en-GB', { ignore: "'-" })
+    .withMessage(
+      'Last name has invalid characters (a-z, A-Z, hyphenation and apostrophes only).'
+    );
+};
 
-const strongPassword = body('password')
-  .trim()
-  .isStrongPassword({
-    minLength: 8,
-    minLowercase: 1,
-    minUppercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-    returnScore: false,
-    pointsPerUnique: 1,
-    pointsPerRepeat: 0.5,
-    pointsForContainingLower: 10,
-    pointsForContainingUpper: 10,
-    pointsForContainingNumber: 10,
-    pointsForContainingSymbol: 10,
-  })
-  .withMessage(
-    'A strong password must be provided. Minimum length 8 characters including at least 1 lower case and 1 upper case character, 1 number and 1 symbol.'
-  );
+const email = () => {
+  return body('email')
+    .isEmail()
+    .withMessage('The provided email address is invalid.');
+};
 
-export default { firstName, lastName, email, strongPassword };
+const newPassword = () => {
+  return body('newPassword')
+    .trim()
+    .isStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+      returnScore: false,
+      pointsPerUnique: 1,
+      pointsPerRepeat: 0.5,
+      pointsForContainingLower: 10,
+      pointsForContainingUpper: 10,
+      pointsForContainingNumber: 10,
+      pointsForContainingSymbol: 10,
+    })
+    .withMessage(
+      'A strong new password must be provided. Minimum length 8 characters including at least 1 lower case and 1 upper case character, 1 number and 1 symbol.'
+    );
+};
+
+const currentPassword = () => {
+  return body('currentPassword')
+    .exists()
+    .withMessage('Your current password must be provided.')
+    .trim();
+};
+
+export default { firstName, lastName, email, currentPassword, newPassword };
