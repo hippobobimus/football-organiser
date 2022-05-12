fe469c3ddd9195b18390a51b8630f89b1bddbe2e
Rob Masters <rjcmasters@gmail.com>
2022-05-12 10:51:44 +0100

    created a formik-based text input component

---
+++ frontend/src/components/form/TextInput.js
@@ -0,0 +1,20 @@
+import { useField } from 'formik';
+import * as Styled from './TextInput.styles';
+
+const TextInput = ({ label, ...props}) => {
+  const [field, meta] = useField(props);
+
+  const isInvalid = meta.touched && meta.error;
+
+  return (
+    <Styled.InputContainer>
+      <Styled.Label htmlFor={props.id || props.name }>{label}</Styled.Label>
+      <Styled.Input {...field} {...props} isInvalid={isInvalid}/>
+      {isInvalid ? (
+        <Styled.ErrorMessage>{meta.error}</Styled.ErrorMessage>
+      ) : null}
+    </Styled.InputContainer>
+  );
+};
+
+export default TextInput;
