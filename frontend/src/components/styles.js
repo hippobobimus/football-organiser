8fe6aa0259feba60777b549a46e6f088c5400f99
Rob Masters <rjcmasters@gmail.com>
2022-05-17 16:29:04 +0100

    added a Section component

--- frontend/src/components/styles.js
+++ frontend/src/components/styles.js
@@ -103,3 +103,11 @@ export const SectionHeading = styled.h3`
   white-space: nowrap;
   align-text: center;
 `;
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+`;
