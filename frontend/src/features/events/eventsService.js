0052291d47c690f3471dbdba8f00740f96154c6c
Rob Masters <rjcmasters@gmail.com>
2022-05-18 10:42:48 +0100

    added thunk func and service to get next match from backend

---
+++ frontend/src/features/events/eventsService.js
@@ -0,0 +1,25 @@
+const API_URL = '/api/events/';
+
+const getNextMatch = async (token) => {
+  const response = await fetch(API_URL + 'next-match', {
+    method: 'GET',
+    headers: {
+      Authorization: token,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await response.json();
+
+  // fetch does not reject on http error status codes, must be handled separately.
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+};
+
+const eventsService = { getNextMatch };
+
+export default eventsService;
--- frontend/src/features/events/eventsSlice.js
+++ frontend/src/features/events/eventsSlice.js
@@ -1,6 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import eventsService from './eventsService';
+
+export const getNextMatch = createAsyncThunk(
+  'events/getNextMatch',
+  async (_, thunkAPI) => {
+    try {
+      return await eventsService.getNextMatch(thunkAPI.getState().auth.token);
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
 
 const initialState = {
+  nextMatch: null,
   status: 'idle',
   message: '',
 };
@@ -10,11 +24,25 @@ const eventsSlice = createSlice({
   initialState,
   reducers: {
     reset: (state) => {
+      state.nextMatch = null;
       state.status = 'idle';
       state.message = '';
     },
   },
-  extraReducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getNextMatch.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getNextMatch.fulfilled, (state, action) => {
+        state.status = 'success';
+        state.nextMatch = action.payload;
+      })
+      .addCase(getNextMatch.rejected, (state, action) => {
+        state.status = 'error';
+        state.message = action.payload;
+      });
+  },
 });
 
 export const { reset } = eventsSlice.actions;
