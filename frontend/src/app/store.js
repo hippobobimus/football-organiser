a91b09d93ce1acff5cf75c76994eaf76f44f8100
Rob Masters <rjcmasters@gmail.com>
2022-05-30 11:35:48 +0100

    added an action to purge the entire store state

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '../features/auth/authSlice';
 import currentUserReducer from '../features/currentUser/currentUserSlice';
@@ -6,12 +6,22 @@ import eventsReducer from '../features/events/eventsSlice';
 import attendeesReducer from '../features/events/attendees/attendeesSlice';
 import usersReducer from '../features/users/usersSlice';
 
+const combinedReducer = combineReducers({
+  auth: authReducer,
+  currentUser: currentUserReducer,
+  events: eventsReducer,
+  attendees: attendeesReducer,
+  users: usersReducer,
+});
+
+const rootReducer = (state, action) => {
+  if (action.type === 'store/purge') {
+    state = undefined;
+  }
+
+  return combinedReducer(state, action);
+};
+
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    currentUser: currentUserReducer,
-    events: eventsReducer,
-    attendees: attendeesReducer,
-    users: usersReducer,
-  },
+  reducer: rootReducer,
 });
