d8b344126606a5d14c9a6dab8b8ab9dc4c70996f
Rob Masters <rjcmasters@gmail.com>
2022-06-13 11:45:53 +0100

    prepared for initial deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,6 +35,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/events', eventsRouter);
 app.use('/api/users', usersRouter);
 
+// frontend
+if (process.env.NODE_ENV === 'production') {
+  const buildDir = new URL('../../frontend/build', import.meta.url);
+  const indexFile = new URL('../../frontend/build/index.html', import.meta.url);
+
+  app.use(express.static(buildDir.pathname));
+
+  app.get('*', (req, res) => {
+    res.sendFile(indexFile.pathname);
+  });
+} else {
+  app.get('/', (req, res) => res.send('Running in development mode'));
+}
+
 // Forward 404 requests to error handler
 app.use((req, res, next) => {
   next(createError(404, 'Bad URI: This resource does not exist'));
