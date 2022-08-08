6d125e225ea4869029f17f8b956fe4a82e37b124
Rob Masters <rjcmasters@gmail.com>
2022-08-08 10:41:55 +0100

    Refactored login page; converted to use RTK Query, toast error notification and split form into separate component

---
+++ frontend/src/features/auth/components/LoginForm.js
@@ -0,0 +1,56 @@
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+import { FormStep, MultiStepForm, TextInput } from '../../../components/form';
+import { Container, Link } from '../../../components/styles';
+import { loginSchema } from '../authUserValidation';
+import { useLoginMutation } from '../../api/apiSlice';
+
+export const LoginForm = ({ onSuccess }) => {
+  const [login, { isError, error, isSuccess }] = useLoginMutation();
+
+  useEffect(() => {
+    if (isSuccess) {
+      onSuccess();
+    } else if (isError) {
+      toast.error(error.data.message);
+    }
+  }, [isSuccess, isError, error, onSuccess]);
+
+  const handleSubmit = async (values, actions) => {
+    try {
+      await login(values);
+    } catch (err) {
+      toast.error(error.data?.message || error.message);
+    }
+
+    actions.resetForm({
+      values: {
+        email: values.email,
+        currentPassword: '',
+      },
+    });
+  };
+
+  return (
+    <>
+      <MultiStepForm
+        initialValues={{
+          email: '',
+          currentPassword: '',
+        }}
+        onSubmit={handleSubmit}
+        submitLabel="Login"
+      >
+        <FormStep validationSchema={loginSchema}>
+          <TextInput label="Email" name="email" type="email" />
+          <TextInput label="Password" name="currentPassword" type="password" />
+        </FormStep>
+      </MultiStepForm>
+      <Container>
+        <p>Not registered yet?</p>
+        <Link to="/register">Create an account</Link>
+      </Container>
+    </>
+  );
+};
--- frontend/src/features/auth/routes/Login.js
+++ frontend/src/features/auth/routes/Login.js
@@ -1,77 +1,22 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { FormStep, MultiStepForm, TextInput } from '../../../components/form';
-import Spinner from '../../../components/spinner/Spinner';
-import { Button, Container, Link, Subtitle } from '../../../components/styles';
-import { login, reset } from '../stores/authSlice';
-import { loginSchema } from '../authUserValidation';
+import { Subtitle } from '../../../components/styles';
+import { LoginForm } from '../components/LoginForm';
 
-const Login = () => {
-  const dispatch = useDispatch();
+export const Login = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const [userEmail, setUserEmail] = useState('');
 
-  // page the user was trying to access.
   const from = location.state?.from?.pathname || '/';
 
-  const { isLoggedIn, status, message } = useSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (isLoggedIn || status === 'success') {
-      // return to page user was trying to access, do not store login page in history.
-      navigate(from, { replace: true });
-      dispatch(reset());
-    }
-  }, [from, isLoggedIn, status, dispatch, navigate]);
-
-  const handleSubmit = (values) => {
-    // store entered email in order to prefill form if login fails and
-    // needs to be reattempted.
-    setUserEmail(values.email);
-    dispatch(login(values));
+  const handleSuccess = () => {
+    navigate(from, { replace: true });
   };
-
-  if (status === 'error') {
-    return (
-      <>
-        <Subtitle>Something went wrong...</Subtitle>
-        <p>{message}</p>
-        <Button type="button" onClick={() => dispatch(reset())}>
-          Back
-        </Button>
-      </>
-    );
-  }
-
-  if (status === 'loading') {
-    return <Spinner />;
-  }
 
   return (
     <>
       <Subtitle>Please Login</Subtitle>
-      <MultiStepForm
-        initialValues={{
-          email: userEmail,
-          currentPassword: '',
-        }}
-        onSubmit={handleSubmit}
-        submitLabel="Login"
-      >
-        <FormStep validationSchema={loginSchema}>
-          <TextInput label="Email" name="email" type="email" />
-          <TextInput label="Password" name="currentPassword" type="password" />
-        </FormStep>
-      </MultiStepForm>
-      <Container>
-        <p>Not registered yet?</p>
-        <Link to="/register">Create an account</Link>
-      </Container>
+      <LoginForm onSuccess={handleSuccess} />
     </>
   );
 };
-
-export default Login;
