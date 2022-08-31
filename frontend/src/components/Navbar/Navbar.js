f55861d726de88652aa696909a495a735d335b22
Rob Masters <rjcmasters@gmail.com>
2022-08-31 16:39:18 +0100

    updated navbar to remove old redux interactions

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,61 +1,63 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import { mdiMenu } from '@mdi/js';
 
 import * as Styled from './Navbar.styles';
 import { Container } from '../styles';
-import Menu from '../menu/Menu';
-import { reset } from '../../features/events/eventsSlice';
+import { Menu } from '../Menu';
+import { useSelector } from 'react-redux';
+
+const NavMenu = ({ menuItems, isRow, onClick }) => {
+  return (
+    <Styled.MenuContainer isRow={isRow}>
+      <Menu isRow={isRow} items={menuItems} onClick={onClick} />
+    </Styled.MenuContainer>
+  );
+};
+
+const Logo = ({ title }) => {
+  return <Styled.NavTitle>{title}</Styled.NavTitle>;
+};
+
+const Toggle = ({ onClick }) => {
+  return <Styled.MenuToggle path={mdiMenu} title="Menu" onClick={onClick} />;
+};
 
 export const Navbar = ({ title, menuItems, widthBreakpoint }) => {
-  const dispatch = useDispatch();
-  const [menuIsVisible, setMenuIsVisible] = useState(false);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const [isMobile, setIsMobile] = useState(false);
-  const { pathname } = useLocation();
+
+  const isCollapsed = screenWidth < widthBreakpoint;
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showToggle = isLoggedIn && isCollapsed;
+  const showMenu = isLoggedIn && (!isCollapsed || isOpen);
 
   useEffect(() => {
     const updateWidth = () => setScreenWidth(window.innerWidth);
-
     window.addEventListener('resize', updateWidth);
-
     return () => window.removeEventListener('resize', updateWidth);
   }, []);
 
-  useEffect(() => {
-    setIsMobile(screenWidth < widthBreakpoint);
-  }, [screenWidth, widthBreakpoint]);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-  useEffect(() => {
-    // ensure menu is initially hidden when switching to mobile view.
-    if (isMobile) {
-      setMenuIsVisible(false);
-    }
-  }, [isMobile]);
-
-  useEffect(() => {
-    // close menu on path change.
-    setMenuIsVisible(false);
-    dispatch(reset());
-  }, [dispatch, pathname]);
-
-  const toggleMenu = () => {
-    setMenuIsVisible(!menuIsVisible);
+  const handleMenuClick = () => {
+    setIsOpen(false);
   };
 
   return (
-    <Styled.Nav isMobile={isMobile}>
+    <Styled.Nav isMobile={isCollapsed}>
       <Container>
-        {isMobile && (
-          <Styled.MenuToggle path={mdiMenu} title="Menu" onClick={toggleMenu} />
-        )}
-        <Styled.NavTitle>{title}</Styled.NavTitle>
+        {showToggle && <Toggle onClick={handleToggle} />}
+        <Logo title={title} />
       </Container>
-      {(menuIsVisible || !isMobile) && (
-        <Styled.MenuContainer isRow={!isMobile}>
-          <Menu isRow={!isMobile} items={menuItems} />
-        </Styled.MenuContainer>
+      {showMenu && (
+        <NavMenu
+          menuItems={menuItems}
+          isRow={!isCollapsed}
+          onClick={handleMenuClick}
+        />
       )}
     </Styled.Nav>
   );
