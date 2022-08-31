4eceebd1dd60194e46ddc2cf9b04f7ab776642b2
Rob Masters <rjcmasters@gmail.com>
2022-08-31 16:56:25 +0100

    hide event admin panel if user is not admin

--- frontend/src/features/events/components/Layout/EventTabLayout.js
+++ frontend/src/features/events/components/Layout/EventTabLayout.js
@@ -2,14 +2,17 @@ import { Outlet } from 'react-router-dom';
 
 import { Tabs } from './Tabs';
 import { EventAdminPanel } from '../Admin';
+import { useGetAuthUserQuery } from '../../../auth/api/authApiSlice';
 
 export const EventTabLayout = ({ navItems }) => {
+  const { data: user } = useGetAuthUserQuery();
+
   return (
     <>
       <Tabs navItems={navItems}>
         <Outlet />
       </Tabs>
-      <EventAdminPanel />
+      {user?.isAdmin && <EventAdminPanel />}
     </>
   );
 };
